ec22fc579085fec9e5c5bd30cd87a6772d8284cc
Suunnys <umarmatraimov208@gmail.com>
2023-10-02 21:17:18 +0600

    homework

--- handlers/fsm_form.py
+++ handlers/fsm_form.py
@@ -2,7 +2,7 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
-from config import bot, DESTINATION_DIR
+from config import bot,DESTINATION_DIR
 from database.sql_commands import Database
 from keybords.inline_buttons import my_profile_keyboard
 
